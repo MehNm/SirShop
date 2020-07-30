2604661b3e259cca8136bc126772feeed22d523d
MehNm <mehnm13@gmail.com>
2020-07-30 11:51:31 -0700

    Updates

---
+++ precache-manifest.908d29af650e2d1bd63bcd3e57c3ab71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40fb868ce7d2f2b14e9862b1b252593",
+    "url": "/SirShopping/index.html"
+  },
+  {
+    "revision": "867f6854c14568756fe6",
+    "url": "/SirShopping/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "46319bf35b4ab86646b7",
+    "url": "/SirShopping/static/css/main.206d0111.chunk.css"
+  },
+  {
+    "revision": "867f6854c14568756fe6",
+    "url": "/SirShopping/static/js/2.dac908a6.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/SirShopping/static/js/2.dac908a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46319bf35b4ab86646b7",
+    "url": "/SirShopping/static/js/main.7ab5758b.chunk.js"
+  },
+  {
+    "revision": "16667c8c382cbabd18b3",
+    "url": "/SirShopping/static/js/runtime-main.26239158.js"
+  }
+]);
